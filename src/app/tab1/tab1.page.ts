c35aed13f5cb2b91a82b64c5008e00f1c6f8290e
arno <atrigallez@syment.com>
2021-04-25 15:48:15 +0200

    fetch offer & display in a list

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpResponse,
+  HttpEvent,
+} from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  private offers: any = [];
+  private loading: boolean = true;
 
-  constructor() {}
+  constructor(protected httpClient: HttpClient) {}
 
+  loadOffer() {
+    // fetch offers
+    this.httpClient
+      .get('http://emploi.nat.tn/fo/Fr/dynamique/api/offre.php')
+      .subscribe((offers) => {
+        console.log('offre reçu:', offers);
+        this.offers = offers;
+        this.loading = false;
+      });
+  }
+
+  ionViewWillEnter() {}
+  clickOnFirstOffer() {}
 }
